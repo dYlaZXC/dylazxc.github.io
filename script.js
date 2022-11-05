0f74c471baf95ebd3f5c55e48b6a22810b1cd52d
adilan <adilan.akhramovich@gmail.com>
2022-11-05 22:15:07 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const options = {
+    bottom: '64px',
+    right: 'unset',
+    left: '32px',
+    time: '0.5s',
+    mixcolor: '#fff',
+    backgroundColor: '#fff',
+    buttonColorDark: '#100f2c',
+    buttonColorRight: '#fff',
+    saveInCookies: false,
+    label: '🌓'
+} 
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
